f92fb2427f18ff09d71a76f9d5f41fda3e309cd1
jakshob <jakshob@hotmail.com>
2019-10-15 11:35:35 +0200

    Mesh with image + 3d models on tween timeline

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ var raycaster;
 var mouse;
 var textureList = [];
 var meshList = [];
+var imgMeshList = [];
 init();
-//animate();
+animate();
 
 function init() {
 	scene = new THREE.Scene();
@@ -48,7 +49,7 @@ var random = makeid(5);*/
 
 	var request = new XMLHttpRequest();
 	
-	request.open('GET', 'https://api.smk.dk/api/v1/art/search/?keys=lundbye&offset=0&rows=10', true);
+	request.open('GET', 'https://api.smk.dk/api/v1/art/search/?keys=lundbye&offset=0&rows=1', true);
 	request.onload = function() {
 		// Begin accessing JSON data here
 
@@ -73,30 +74,37 @@ var random = makeid(5);*/
 			app.appendChild(errorMessage);
 		}
 
+		//Spheres
+		var sphereGeometry = new THREE.BoxBufferGeometry(40, 40, 40);
+		for (var i = 0; i < textureList.length; i++) {
+			console.log(textureList[i]);
+			var textureApi = new THREE.TextureLoader().load(textureList[i]);
+			var sphereMaterial = new THREE.MeshPhongMaterial({
+				specular: 0xffffff,
+				flatShading: true,
+				map: textureApi
+			});
+			var sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+			sphereMesh.position.set(2, 2, -100);
+			sphereMesh.rotation.set(2 * i, 2 * i, 4 * i);
+			scene.add(sphereMesh)
+			imgMeshList.push(sphereMesh);
+		}
 
 	}
 	request.send();
 
 	//LOADING 3D objects
 	var loader = new THREE.STLLoader();
-
-
-
+	
 	var models = ['./models/poseidon.stl', './models/davidAndGoliath.stl', './models/headOfDavid.stl', './models/venus.stl', './models/madonna.stl', './models/laocoon.stl'];
 
 	var material = new THREE.MeshLambertMaterial({ color: 0xF7F7F7 });
-
-
+	
 	var count = 0;
-	/*
-	forEachAsync(models, function(next, modelPath) {
 	
-	
-	});*/
-
-
 	for (var i = 0; i < models.length; i++) {
-		console.log(models[1]);
+		
 
 		loader.load(models[i], function (geometry) {
 			console.log(models[i]);
@@ -109,13 +117,11 @@ var random = makeid(5);*/
 			meshList.push(mesh);
 			count++;
 		});
-
-
-
 	}
 
+	
 
-
+	//LIGHTS
 	var light = new THREE.PointLight(0xFF0000, 1, 1000)
 	light.position.set(0, 0, -55);
 	scene.add(light);
@@ -140,43 +146,29 @@ function getNonZeroRandomNumber() {
 
 function onMouseMove(event) {
 	event.preventDefault();
-
+	
 	console.log(textureList);
 
 	mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
 	mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
-
-	var bool = true;
-
-	/*
-	loader.load('./models/headOfDavid.stl', function (geometry1) {
-		console.log(geometry1);
-		var material1 = new THREE.MeshLambertMaterial({ color: 0xF7F7F7 });
-		var mesh2 = new THREE.Mesh(geometry1, material1);
-		mesh2.position.set(0, 0, -400);
-		mesh2.scale.set(1, 1, 1);
-
-		
-
-		console.log(mesh2.position);
-		//scene.add(mesh2);
-		scene.add(mesh2);
-
-	});
-	*/
 	
 	for (var i = 0; i < meshList.length; i++) {
 		this.tl = new TimelineMax().delay(.3);
-		this.tl.to(meshList[i].rotation, 2, { x: 1, ease: Expo.easeOut })
+		this.tl.to(meshList[i].rotation, 2, { x: 1*i, ease: Expo.easeOut })
 			.to(meshList[i].position, 20, { y: -200, ease: Expo.easeOut }, 0)
 			.to(meshList[i].rotation, 20, { y: 1, ease: Expo.easeOut }, 2)
 			.to(meshList[i].rotation, 20, { x: -1, ease: Expo.easeOut }, 12)
 
 	}
 
+	for (var i = 0; i < imgMeshList.length; i++) {
+		this.tl = new TimelineMax().delay(.3);
+		this.tl.to(imgMeshList[i].rotation, 20, { x: 4, ease: Expo.easeOut })
+			
+
+	}
+
 
 }
 
-
 window.addEventListener('click', onMouseMove);
-animate();
